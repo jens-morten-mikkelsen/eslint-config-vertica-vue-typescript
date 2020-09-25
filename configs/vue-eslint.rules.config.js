4b061aff75e44ba2325fd12806ec9db97a8f0549
Jens Morten Mikkelsen <jmm@vertica.dk>
2020-09-25 09:53:15 +0200

    v0.6.0 Removed unnecessary redefinition of default options for "vue/order-in-components" and reimplemented rules from v7 of "eslint-plugin-vue"

--- configs/vue-eslint.rules.config.js
+++ configs/vue-eslint.rules.config.js
@@ -73,43 +73,7 @@ module.exports = {
         'vue/component-tags-order': ['error', {
             'order': ['template', 'script', 'style']
         }],
-        'vue/order-in-components': ['error', {
-            'order': [
-                'el',
-                'name',
-                'key',
-                'parent',
-                'functional',
-                ['delimiters', 'comments'],
-                ['components', 'directives', 'filters'],
-                'extends',
-                'mixins',
-                ['provide', 'inject'],
-                'ROUTER_GUARDS',
-                'layout',
-                'middleware',
-                'validate',
-                'scrollToTop',
-                'transition',
-                'loading',
-                'inheritAttrs',
-                'model',
-                ['props', 'propsData'],
-                'emits',
-                'setup',
-                'asyncData',
-                'data',
-                'fetch',
-                'head',
-                'computed',
-                'watch',
-                'watchQuery',
-                'LIFECYCLE_HOOKS',
-                'methods',
-                ['template', 'render'],
-                'renderError'
-            ]
-        }],
+        'vue/order-in-components': ['error'],
         // endregion
 
         // region UnCategorized
